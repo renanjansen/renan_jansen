1e8b35a80a7f51af6e3a11489649ca170b3ee209
Renan Jansen <renan.jansen@dbseller.com.br>
2023-10-10 23:53:43 -0300

    fix: definitivo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 
-import { createApp } from 'vue/dist/vue.esm-bundler.js'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
